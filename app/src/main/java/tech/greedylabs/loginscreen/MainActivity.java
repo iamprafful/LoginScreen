1c5f26b0923884432dd42736ac0f130fd7c1e320
Prafful <prafful.lachhwani@gmail.com>
2020-06-23 12:39:00 +0530

    Initial commit

---
+++ app/src/main/java/tech/greedylabs/loginscreen/MainActivity.java
@@ -0,0 +1,14 @@
+package tech.greedylabs.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
